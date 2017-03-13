2a0bb48e34a3c79c6e48ce5d33722ec96de9afe6
Konpat Preechakul <the.akita.ta@gmail.com>
2017-03-14 01:09:28 +0700

    fix button size; reformat; fix image not center

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,32 @@
-@extends('layouts.app')
-@section('content')
-    <div id="appHome" class="container-fluid">
-        <div class="row">
-             <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-                <img src="img/logo.svg" alt=" " class="img-responsive" />
+@extends('layouts.app') @section('content')
+<div id="appHome" class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+      <div class="text-center">
+        <!-- แก้ logo ไม่กลางด้วยการใส่ display: inline แล้วครอบด้วย div ให้เป็น block อีกที -->
+        <img src="img/logo.svg" alt=" " class="img-responsive" style="display: inline;">
+      </div>
 
-                <div class="center-block fb-login">  
-                <a href ="{{ route('facebook-login') }}">
-                <div class="fb-btn">
-                    <div class="fb-btn-shadow"></div>
-                    <div class="fb-btn-background"></div>
-                    <div class="fb-btn-border"></div>
-                    <div class="fb-btn-label">
-                        <div class="fb-btn-content">
-                            <div class="fb-btn-space"></div>
-                            <div class="fb-btn-text">
-                                Login with Facebook
-                            </div>
-                        </div>
-                    </div>
-               
+      <div class="fb-login">
+        <a href="{{ route('facebook-login') }}">
+          <div class="fb-btn">
+            <div class="fb-btn-shadow"></div>
+            <div class="fb-btn-background"></div>
+            <div class="fb-btn-border"></div>
+            <div class="fb-btn-label">
+              <div class="fb-btn-content">
+                <div class="fb-btn-space"></div>
+                <div class="fb-btn-text">
+                  Login with Facebook
                 </div>
-                </a>
-                </div>
-               
-              </div>              
-        </div>
+              </div>
+            </div>
+
+          </div>
+        </a>
+      </div>
+
     </div>
+  </div>
+</div>
 @endsection
