4cb1dfa20ffbb03d14d26488312b5c19d1b0a77d
Pichet Wongbuakaew <pichet.nk@gmail.com>
2017-03-16 06:47:11 +0700

    bulie production

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends('layouts.app') @section('content')
 <div id="team" class="container-fluid">
   <div class="team-bg">
@@ -11,47 +10,16 @@
       @include('profileimage')
     </div>
     <div class="col-lg-4 col-lg-offset-1 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 col-team">
-      <img src="/img/{{$teamImg}}.svg" class="img-responsive">
-      <div class="profile-name">
+        <div class="team-show">
+            <img src="/img/{{$teamImg}}.svg" class="img-responsive">
+            <div class="profile-name">
 
-        {{$userDetail->nickname}} @if ( $userDetail->generation == 0) สมาคม @else #YWC{{$userDetail->generation}} @endif
+              {{$userDetail->nickname}} @if ( $userDetail->generation == 0) สมาคม @else #YWC{{$userDetail->generation}} @endif
 
-      </div>
-    </div>
-  </div>
-  <div class="row team">
-    <div class="row team-red">
-
-
-      <div class="row">
-        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-          <img src="/img/team_red.svg" class="img-responsive team-banner" alt="">
-=======
-@extends('layouts.app')
-@section('content')
-    <div id="team" class="container-fluid">
-        <div class="row profile">
-             <div class="col-lg-4 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                       
-                 @include('profileimage')     
-                </div> 
-                <div class="col-lg-4 col-lg-offset-1 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 col-team">
-                       <div class="team-show">
-                            <img src="/img/{{$teamImg}}.svg" class="img-responsive">    
-                            <div class="profile-name">
-                                
-                                {{$userDetail->nickname}}
-                                @if ( $userDetail->generation  == 0)
-                                    สมาคม
-                                @else
-                                #YWC{{$userDetail->generation}}
-                                @endif  
-                                    
-                            </div>
-                       </div>
-
-                      <div class="btn-order">
-                          <div class="btn-col">
+            </div>
+        </div>
+         <div class="btn-order">
+                <div class="btn-col">
                              <div class="btn-box">
                                         <div class="btn-box-border"></div>
                                         <div class="btn-box-bg"></div>
@@ -62,16 +30,18 @@
                                         </div>
                                 </div>   
                               </div> 
-                      </div>
+         </div>
 
 
+    </div>
+  </div>
+  <div class="row team">
+    <div class="row team-red">
 
-                </div> 
-        </div>
-        <div class="row team">
-        <div class="team-bg">
-             <div class="team-bg-img"></div>
->>>>>>> 28eef00070f382f6cb1ba146c48720448b9663f2
+
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_red.svg" class="img-responsive team-banner" alt="">
         </div>
       </div>
       <div class="row">
