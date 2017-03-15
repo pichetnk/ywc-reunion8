3f380529b31dda174a014aec4655d4961d3b95b1
Konpat Preechakul <the.akita.ta@gmail.com>
2017-03-16 01:05:56 +0700

    fix team covering texture

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,181 +1,172 @@
-@extends('layouts.app')
-@section('content')
-    <div id="team" class="container-fluid">
-        <div class="row profile">
-             <div class="col-lg-4 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                       
-                 @include('profileimage')     
-                </div> 
-                <div class="col-lg-4 col-lg-offset-1 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 col-team">
-                        <img src="/img/{{$teamImg}}.svg" class="img-responsive">    
-                        <div class="profile-name">
-                            
-                            {{$userDetail->nickname}}
-                            @if ( $userDetail->generation  == 0)
-                                สมาคม
-                            @else
-                            #YWC{{$userDetail->generation}}
-                            @endif  
-                                
-                        </div>
-                </div> 
+@extends('layouts.app') @section('content')
+<div id="team" class="container-fluid">
+  <div class="team-bg">
+    <div class="team-bg-img"></div>
+  </div>
+
+  <div class="row profile">
+    <div class="col-lg-4 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+
+      @include('profileimage')
+    </div>
+    <div class="col-lg-4 col-lg-offset-1 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 col-team">
+      <img src="/img/{{$teamImg}}.svg" class="img-responsive">
+      <div class="profile-name">
+
+        {{$userDetail->nickname}} @if ( $userDetail->generation == 0) สมาคม @else #YWC{{$userDetail->generation}} @endif
+
+      </div>
+    </div>
+  </div>
+  <div class="row team">
+    <div class="row team-red">
+
+
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_red.svg" class="img-responsive team-banner" alt="">
         </div>
-        <div class="row team">
-        <div class="team-bg">
-             <div class="team-bg-img"></div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+
+
+
+
+          @foreach ($teamRed as $user)
+          <div class="member">
+
+            <div class="pic-frame">
+              <div class="pic-frame-border-out"></div>
+              <div class="pic-frame-icon"> </div>
+
+              <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
+
+              <div class="pic-frame-border-in"></div>
+            </div>
+          </div>
+          @endforeach
+
+
+
         </div>
-       
-         <div class="row team-red">  
-         
-            
-            <div class="row">
-                <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-                      <img src="/img/team_red.svg" class="img-responsive team-banner" alt="">
-                </div>   
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                          
-
-                     
-                      
-                     @foreach ($teamRed as $user)
-                          <div class="member"> 
-                          
-                           <div class="pic-frame">
-                               <div class="pic-frame-border-out"></div>
-                               <div class="pic-frame-icon"> </div>                             
-                             
-                               <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
-                              
-                           <div class="pic-frame-border-in"></div>
-                         </div>
-                      </div>
-                     @endforeach
-
-                             
-                      
-                </div>   
-            </div>
-            <div class="row buttomTeam"></div>
-         </div>
-
-          <div class="row team-blue">  
-            <div class="row">
-                <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-                      <img src="/img/team_blue.svg" class="img-responsive team-banner" alt="">
-                </div>   
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-
-                       @foreach ($teamBlue as $user)
-                          <div class="member"> 
-                           <div class="pic-frame">
-                               <div class="pic-frame-border-out"></div>
-                               <div class="pic-frame-icon"></div>                             
-                             
-                               <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
-                              
-                           <div class="pic-frame-border-in"></div>
-                         </div>
-                      </div>
-                     @endforeach
-
-                </div>   
-            
-            </div>
-            <div class="row buttomTeam"></div>
-         </div>
-
-          <div class="row team-green">  
-            <div class="row">
-                <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-                      <img src="/img/team_green.svg" class="img-responsive team-banner" alt="">
-                </div>   
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                     @foreach ($teamGreen as $user)
-                          <div class="member"> 
-                          
-                           <div class="pic-frame">
-                               <div class="pic-frame-border-out"></div>
-                               <div class="pic-frame-icon"></div>                             
-                             
-                               <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
-                              
-                           <div class="pic-frame-border-in"></div>
-                          
-                         </div>
-                           <div class="labal-name">
-                                  {{ $user->nickname }} 
-                                  @if ($user->generation !=0 )
-                                     #{{$user->generation}}
-                                  @endif
-                           </div>
-                      </div>
-                     @endforeach
-
-                </div>   
-            </div>
-            <div class="row buttomTeam"></div>
-         </div>
-
-
-          <div class="row team-orange">  
-            <div class="row">
-                <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-                      <img src="/img/team_orange.svg" class="img-responsive team-banner" alt="">
-                </div>   
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                      
-                        @foreach ($teamOrange as $user)
-                          <div class="member"> 
-                           <div class="pic-frame">
-                               <div class="pic-frame-border-out"></div>
-                               <div class="pic-frame-icon"></div>                             
-                             
-                               <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
-                              
-                           <div class="pic-frame-border-in"></div>
-                         </div>
-                      </div>
-                     @endforeach
-                </div>   
-            </div>
-            <div class="row buttomTeam"></div>
-         </div>
-
-          <div class="row team-yellow">  
-            <div class="row">
-                <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
-                      <img src="/img/team_yellow.svg" class="img-responsive team-banner" alt="">
-                </div>   
-            </div>
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
-                      @foreach ($teamYellow as $user)
-                          <div class="member"> 
-                           <div class="pic-frame">
-                               <div class="pic-frame-border-out"></div>
-                               <div class="pic-frame-icon"></div>                           
-                             
-                               <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
-                              
-                           <div class="pic-frame-border-in"></div>
-                         </div>
-                      </div>
-                     @endforeach
-
-                </div>   
-            </div>
-            <div class="row buttomTeam"></div>
-         </div>
-
-         </div>
+      </div>
+      <div class="row buttomTeam"></div>
     </div>
 
-    @endsection
+    <div class="row team-blue">
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_blue.svg" class="img-responsive team-banner" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+
+          @foreach ($teamBlue as $user)
+          <div class="member">
+            <div class="pic-frame">
+              <div class="pic-frame-border-out"></div>
+              <div class="pic-frame-icon"></div>
+
+              <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
+
+              <div class="pic-frame-border-in"></div>
+            </div>
+          </div>
+          @endforeach
+
+        </div>
+
+      </div>
+      <div class="row buttomTeam"></div>
+    </div>
+
+    <div class="row team-green">
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_green.svg" class="img-responsive team-banner" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+          @foreach ($teamGreen as $user)
+          <div class="member">
+
+            <div class="pic-frame">
+              <div class="pic-frame-border-out"></div>
+              <div class="pic-frame-icon"></div>
+
+              <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
+
+              <div class="pic-frame-border-in"></div>
+
+            </div>
+            <div class="labal-name">
+              {{ $user->nickname }} @if ($user->generation !=0 ) #{{$user->generation}} @endif
+            </div>
+          </div>
+          @endforeach
+
+        </div>
+      </div>
+      <div class="row buttomTeam"></div>
+    </div>
+
+
+    <div class="row team-orange">
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_orange.svg" class="img-responsive team-banner" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+
+          @foreach ($teamOrange as $user)
+          <div class="member">
+            <div class="pic-frame">
+              <div class="pic-frame-border-out"></div>
+              <div class="pic-frame-icon"></div>
+
+              <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
+
+              <div class="pic-frame-border-in"></div>
+            </div>
+          </div>
+          @endforeach
+        </div>
+      </div>
+      <div class="row buttomTeam"></div>
+    </div>
+
+    <div class="row team-yellow">
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+          <img src="/img/team_yellow.svg" class="img-responsive team-banner" alt="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+          @foreach ($teamYellow as $user)
+          <div class="member">
+            <div class="pic-frame">
+              <div class="pic-frame-border-out"></div>
+              <div class="pic-frame-icon"></div>
+
+              <img src="//graph.facebook.com/{{ $user->facebook_id }}/picture?type=large" class="pic-frame-pic">
+
+              <div class="pic-frame-border-in"></div>
+            </div>
+          </div>
+          @endforeach
+
+        </div>
+      </div>
+      <div class="row buttomTeam"></div>
+    </div>
+
+  </div>
+</div>
+
+@endsection
