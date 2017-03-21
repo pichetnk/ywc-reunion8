d3c78cbe5bff023c86296a5078e9cbafb5c66e19
Pichet Wongbuakaew <pichet.nk@gmail.com>
2017-03-21 22:10:37 +0700

    update home

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -73,7 +73,7 @@
       <div class="row team-red">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
               <img src="/img/team_red.svg" class="img-responsive team-banner" alt="">
             </div>
           </div>
@@ -113,7 +113,7 @@
       <div class="row team-blue">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
               <img src="/img/team_blue.svg" class="img-responsive team-banner" alt="">
             </div>
           </div>
@@ -148,7 +148,7 @@
       <div class="row team-green">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
               <img src="/img/team_green.svg" class="img-responsive team-banner" alt="">
             </div>
           </div>
@@ -183,7 +183,7 @@
       <div class="row team-orange">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
               <img src="/img/team_orange.svg" class="img-responsive team-banner" alt="">
             </div>
           </div>
@@ -215,7 +215,7 @@
       <div class="row team-yellow">
         <div class="col-md-12">
           <div class="row">
-            <div class="col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
+            <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3">
               <img src="/img/team_yellow.svg" class="img-responsive team-banner" alt="">
             </div>
           </div>
